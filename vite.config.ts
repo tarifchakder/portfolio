9e3c7963fefda8ca24d9f2f95b3f7054655cc6fe
tarif <tarifchakdar@gmail.com>
2025-09-27 09:53:24 +0400

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
+    base: "/portfolio/",
+    server: {
     host: "::",
     port: 8080,
   },
