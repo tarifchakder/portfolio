5eda921210557c8a01f6ca43eb3ee9dee1c5df46
tarif <tarifchakdar@gmail.com>
2025-09-27 10:09:41 +0400

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,22 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-    base: mode === 'production' ? '/portfolio/' : '/',
+// Detect production for GitHub Pages
+const isProd = process.env.NODE_ENV === "production";
+
+export default defineConfig({
+    base: isProd ? "/portfolio/" : "/", // <-- important
     server: {
         host: "::",
         port: 8080,
     },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [
+        react(),
+        !isProd && componentTagger(),
+    ].filter(Boolean),
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
-}));
+});
