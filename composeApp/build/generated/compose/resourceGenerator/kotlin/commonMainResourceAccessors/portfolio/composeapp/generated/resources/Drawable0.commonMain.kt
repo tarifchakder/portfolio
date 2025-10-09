866576a3c6ea55f08146d0d2e4dc93178a66d5d3
tarif <tarifchakdar@gmail.com>
2025-10-09 14:41:13 +0530

    chore: add IDE and deployment configuration files

--- composeApp/build/generated/compose/resourceGenerator/kotlin/commonMainResourceAccessors/portfolio/composeapp/generated/resources/Drawable0.commonMain.kt
+++ composeApp/build/generated/compose/resourceGenerator/kotlin/commonMainResourceAccessors/portfolio/composeapp/generated/resources/Drawable0.commonMain.kt
@@ -3,21 +3,12 @@
 package portfolio.composeapp.generated.resources
 
 import kotlin.OptIn
-import kotlin.String
-import kotlin.collections.MutableMap
-import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.InternalResourceApi
+import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.ResourceItem
+import org.jetbrains.compose.resources.LanguageQualifier
+import org.jetbrains.compose.resources.RegionQualifier
+import org.jetbrains.compose.resources.ThemeQualifier
+import org.jetbrains.compose.resources.DensityQualifier
 
-private const val MD: String = "composeResources/portfolio.composeapp.generated.resources/"
-
-internal val Res.drawable.compose_multiplatform: DrawableResource by lazy {
-      DrawableResource("drawable:compose_multiplatform", setOf(
-        ResourceItem(setOf(), "${MD}drawable/compose-multiplatform.xml", -1, -1),
-      ))
-    }
-
-@InternalResourceApi
-internal fun _collectCommonMainDrawable0Resources(map: MutableMap<String, DrawableResource>) {
-  map.put("compose_multiplatform", Res.drawable.compose_multiplatform)
-}
+internal val Res.drawable.compose_multiplatform: DrawableResource by lazy { DrawableResource("drawable:compose_multiplatform", setOf(ResourceItem(setOf(),"drawable/compose-multiplatform.xml",-1,-1))) }
