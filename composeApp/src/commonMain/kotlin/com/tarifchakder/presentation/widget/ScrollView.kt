40633abe0f34f2623fc74d502f2382d5cc127fac
tarif <tarifchakdar@gmail.com>
2025-10-29 22:22:57 +0400

    add `ScrollView` composable for vertical scrolling support in Compose Multiplatform

---
+++ composeApp/src/commonMain/kotlin/com/tarifchakder/presentation/widget/ScrollView.kt
@@ -0,0 +1,26 @@
+package com.tarifchakder.presentation.widget
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ScrollView(
+    modifier: Modifier = Modifier,
+    verticalArrangement: Arrangement.Vertical = Arrangement.Top,
+    horizontalAlignment: Alignment.Horizontal = Alignment.Start,
+    content: @Composable (ColumnScope.() -> Unit)
+) {
+    val scrollState = rememberScrollState()
+    Column(
+        modifier = modifier.verticalScroll(state = scrollState, enabled = true),
+        verticalArrangement = verticalArrangement,
+        horizontalAlignment = horizontalAlignment,
+        content = content
+    )
+}
