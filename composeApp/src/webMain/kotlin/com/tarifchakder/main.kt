cb5928070cd9fd6c15e3213a3366344ca2407e84
tarif <tarifchakdar@gmail.com>
2026-08-08 00:16:03 +0400

    Simplify ZPlayer terms to static HTML

--- composeApp/src/webMain/kotlin/com/tarifchakder/main.kt
+++ composeApp/src/webMain/kotlin/com/tarifchakder/main.kt
@@ -3,20 +3,10 @@ package com.tarifchakder
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import com.tarifchakder.presentation.App
-import com.tarifchakder.presentation.screen.ZPlayerTermsApp
-import kotlinx.browser.window
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
-    val isZPlayerTermsPage = window.location.pathname
-        .trimEnd('/')
-        .endsWith("/zplayer/terms")
-
     ComposeViewport {
-        if (isZPlayerTermsPage) {
-            ZPlayerTermsApp()
-        } else {
-            App()
-        }
+        App()
     }
 }
